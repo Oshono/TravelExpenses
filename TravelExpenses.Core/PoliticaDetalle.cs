723a7e538e6dfceeb9548de6e231474b8de15e95
Antonio García <xquetzalcoatl@gmail.com>
2019-08-01 22:50:48 -0500

    Working on Politic details

---
+++ TravelExpenses.Core/PoliticaDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelExpenses.Core
+{
+    public class PoliticaDetalle
+    {
+        public PoliticaDetalle()
+        {
+            ImportePermitido = 0;
+            Activo = true;
+        }
+        [Required]
+        public int IdPolitica { get; set; }
+        [Required]
+        public int IdGasto { get; set; }
+        
+        [Range(0.0, float.MaxValue)]
+        public decimal ImportePermitido { get; set; }
+        public bool Activo { get; set; }
+    }
+}
